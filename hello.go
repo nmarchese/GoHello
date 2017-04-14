a10bbae5f9b32cc38bbfd8ed10d33bf13916ed43
Nicholas Marchese <NikM@NikMs-MacBook-Pro.local>
2017-04-14 13:50:16 -0600

    created stringutil package with Reverse function to use in hello.go

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/nmarchese/stringutil"
+  )
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
